f6c4e13b3c585b68b573bb2276164e1c9f0c8dca
tedros <tediousalie@gmail.com>
2024-09-04 12:06:32 -0700

    [index.js]project file implemented

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 // Define the /status and /stats routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// New route to create a user
+router.post('/users', UsersController.postNew);
 
 export default router;
